2c1347bae5a8de9984e5d99db581f8fba12cb281
Adrian Cybulski <adrian@cybulski.cc>
2023-02-11 11:03:35 +0000

    void build for ignite

---
+++ Dockerfile
@@ -0,0 +1,42 @@
+FROM alpine:3.13 as build
+
+RUN mkdir /rootfs
+ADD https://repo-default.voidlinux.org/live/current/void-x86_64-ROOTFS-20221001.tar.xz /rootfs
+WORKDIR /rootfs
+RUN tar xvf *.tar.xz
+RUN rm -rf *.tar.xz
+
+
+FROM ghcr.io/tailscale/tailscale:v1.34.1 as ts
+FROM ghcr.io/netauth/netauth:v0.6.1 as netauth
+FROM grafana/agent:latest as grafana
+
+from golang:1.19-alpine as localizer
+RUN apk add git
+WORKDIR /src
+
+RUN git clone --branch v0.1.3 https://github.com/netauth/localizer /src
+
+RUN go mod vendor && \
+      CGO_ENABLED=0 GOOS=linux go build -a -ldflags '-extldflags "-static"' -o /localize cmd/localize/*.go
+
+FROM scratch
+
+COPY --from=build /rootfs /
+
+RUN xbps-install -Suy xbps
+RUN xbps-install -Sy gettext jq vsv socklog-unix
+
+COPY --from=ts /usr/local/bin/tailscale /bin/tailscale
+COPY --from=ts /usr/local/bin/tailscaled /bin/tailscaled
+COPY --from=grafana /bin/grafana-agent /bin/grafana-agent
+
+COPY --from=netauth /n /bin/netauth
+COPY --from=localizer /localize /bin/localize
+
+COPY ./etc /etc
+RUN chmod 0400 /etc/sudoers
+COPY ./entrypoint.sh /entrypoint
+RUN chmod +x /entrypoint
+
+ENTRYPOINT [ "/entrypoint" ]
---
+++ entrypoint.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+
+export DATA_ROOT=${VOLUME_PATH:-/data}
+
+## 1. Start tailscaled
+
+TS_DATA_DIR="${DATA_ROOT}/tailscale/data"
+TS_RUNTIME="/var/run/tailscale"
+
+# create tailscale data driectories if not present
+[ ! -d "$TS_DATA_DIR" ] && mkdir -p $TS_DATA_DIR
+[ ! -d "$TS_RUNTIME" ] && mkdir -p $TS_RUNTIME
+
+# start tailscale daemon
+/bin/tailscaled \
+  --statedir=$TS_DATA_DIR \
+  --socket=$TS_RUNTIME/tailscaled.sock &
+
+## 2. Login to tailscale
+
+sleep 5; # wait 5s for tailscaled to startup
+/bin/tailscale up \
+  --ssh \
+  --accept-routes \
+  --auth-key "${TSKEY}" \
+  --hostname "${TS_HOSTNAME:-$HOSTNAME}"
+
+FQDN="$(tailscale status --json | jq --raw-output  .Self.DNSName | sed 's/\.$//')"
+
+## 3. Generate certs
+cd /tmp && tailscale cert "${FQDN}"
+
+export TLS_CERT="/tmp/${FQDN}.crt"
+export TLS_KEY="/tmp/${FQDN}.key"
+
+## configure netauth
+NETAUTH_DIR="/etc/netauth"
+[ ! -d "$NETAUTH_DIR/keys" ] && mkdir -p "${NETAUTH_DIR}/keys"
+
+if [ -n "$NETAUTH_SERVER" ]; then
+  # fetch server certificate
+  openssl s_client \
+    -connect "${NETAUTH_SERVER}:1729" 2>/dev/null </dev/null | \
+    sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > "${NETAUTH_DIR}/keys/tls.pem"
+
+  envsubst < "${NETAUTH_DIR}/config.tpl.toml" > "${NETAUTH_DIR}/config.toml"
+
+  # sync users and groups
+  /bin/localize --min-gid 2000 --min-uid 2000
+fi
+
+
+## 5. Start bash or runit
+
+
+RUNIT_SVDIR="/var/service"
+RUNIT_CURRENT="/run/runit/runsvdir/current"
+
+[ ! -d "$RUNIT_CURRENT" ] && mkdir -p "${RUNIT_CURRENT}"
+
+#ln -sf "${RUNIT_CURRENT}" "${RUNIT_SVDIR}"
+find /etc/sv/ -type f -name 'run' | xargs -L1 chmod +x
+ln -sf /etc/sv/grafana ${RUNIT_CURRENT}/grafana
+
+/sbin/runsvdir -P "/run/runit/runsvdir/current" 
